511cb1976622f94788c4996a5ce2130be42469c9
Atul1223 <a1atulk@gmail.com>
2023-02-16 23:37:41 +0530

    Added JatpackCompose Navigation
    Signed-off-by: Atul1223 <a1atulk@gmail.com>

--- app/src/main/java/com/example/notes/MainActivity.kt
+++ app/src/main/java/com/example/notes/MainActivity.kt
@@ -5,27 +5,39 @@ import android.graphics.drawable.Drawable
 import android.graphics.drawable.VectorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.*
+import androidx.navigation.fragment.NavHostFragment
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.fragment_login.*
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var navHostFragment: NavHostFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //showBackground()
-        showAnimation()
-        showPenAnimation()
+        setNavHostNavController()
+        changeFragment()
+    }
+    private fun changeFragment() {
+        if(checkLoginUser()){
+            navController.navigate(R.id.homeFragment)
+        }
+        else{
+            navController.navigate(R.id.loginFragment)
+        }
     }
 
-    private fun showAnimation() {
-        (iv_home_animation.drawable as AnimationDrawable).start()
+    private fun checkLoginUser(): Boolean {
+        return false
     }
 
-    private  fun showPenAnimation() {
-        (ivb_create_note.drawable as AnimationDrawable).start()
+    private fun setNavHostNavController() {
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.main_fragment) as NavHostFragment
+        navController =  navHostFragment.navController
     }
-
-//    private fun showBackground() {
-//        (iv_home_background.drawable as Drawable).setVisible(true,false)
-//    }
 }
